16ae3880b9a4738843367b226f7d53ac9c3ec540
Vigyat18072005 <vigyattomar@gmail.com>
2026-08-19 21:22:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+
+// Mobile menu
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+
+// Close mobile menu after clicking a link
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
+
+
+// Contact form
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    alert("Thank you! Your message has been received.");
+
+    contactForm.reset();
+});
